79129083fc43c26543d51554a1f9d95c4e66debd
Sean Ford <seanthomasford@gmail.com>
2018-01-14 01:41:37 -0500

    init commit

---
+++ wgrass/crops.js
@@ -0,0 +1,9 @@
+
+var cropInstance = {
+	name: "Wheatgrass",
+	seedDate: "seedDate",
+	daysSinceSeed: "daysSinceSeed",
+	daysToHarvest: "daysToHarvest",
+	status: "status"
+	
+};
---
+++ wgrass/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+	$("nav").click(function(){
+		console.log("hi");
+	});
+
+
+});
